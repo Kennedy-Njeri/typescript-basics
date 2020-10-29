92d09ec1c961ce35cf9640162ced2dd6d4b4dd7e
Kennedy Njeri <mistakenz123@gmail.com>
2020-10-29 21:17:10 +0300

    basics

---
+++ fetchjson/index.ts
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+
+
+interface Todo {
+    id: number
+    title: string
+    completed: boolean
+}
+
+
+axios.get(url).then((response => {
+    //console.log(response.data)
+    const todo = response.data as Todo
+
+    const id = todo.id
+    const title = todo.title
+    const completed = todo.completed
+
+
+    //const { id, title, completed } = todo
+    console.log(`The todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}`)
+})).catch((error) => {
+    console.log(error)
+})

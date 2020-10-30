a3673a1b74572cfc360ffdfc8e936c9cd21f5aae
Kennedy Njeri <mistakenz123@gmail.com>
2020-10-30 22:56:56 +0300

    basics

---
+++ features/types.ts
@@ -0,0 +1,20 @@
+const today = new Date()
+
+today.getDate()
+
+
+
+const person = {
+    age: 20
+}
+
+
+person.age
+
+
+
+// categories of types
+// 1. primitive types = numbers, string, void, symbol null
+
+//
+// 2. object types = functions, classes, arrays,  objects

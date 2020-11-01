acbfedb9969516e7485d90e53c7d50fb4f040de9
Kennedy Njeri <mistakenz123@gmail.com>
2020-11-01 13:57:44 +0300

    annotations and destructuring

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -14,3 +14,35 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
     return a * b
 }
+
+
+// void and never
+
+const logger = (message: string): void => {
+    console.log(message)
+}
+
+
+const throwError = (message: string): never => {
+    throw new Error(message)
+}
+
+// destructuring with annotations
+
+const forecast = {
+    date: new Date(),
+    weather: "sunny"
+}
+
+// const logWeather = (forecast: { date: Date; weather: string }): void => {
+//     console.log(forecast.date)
+//     console.log(forecast.weather)
+// }
+
+
+const logWeather = ({ date, weather}: { date: Date; weather: string }): void => {
+    console.log(date)
+    console.log(weather)
+}
+
+logWeather(forecast)

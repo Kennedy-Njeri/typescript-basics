f6d6e7eca6d8007ca2c803d05070fa8c4f094aab
Kennedy Njeri <mistakenz123@gmail.com>
2020-11-01 12:44:46 +0300

    delayed initialization

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -55,3 +55,16 @@ for (let i= 0; i < words.length; i++) {
     }
 }
 
+
+
+// 3. variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12]
+let numberAboveZero: boolean | number = false
+
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i]
+    }
+}
+

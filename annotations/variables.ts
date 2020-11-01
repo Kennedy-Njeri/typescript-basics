a6ab823d231f25e62c355aa7356139491b2b0cd1
Kennedy Njeri <mistakenz123@gmail.com>
2020-11-01 11:41:05 +0300

    object literal annotatations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -9,3 +9,24 @@ let nothing: undefined = undefined
 
 // built in objects
 let now: Date = new Date()
+
+
+// arrays
+let colors: string[] = ["green", "red", "purple"]
+let myNumbers: number[] = [1, 2, 3]
+let truths: boolean[] = [true, false, true]
+
+
+// classes
+
+class Car {
+
+}
+
+let car: Car = new Car()
+
+// objects
+let point: { x: number; y: number } = {
+    x: 30,
+    y: 20
+}

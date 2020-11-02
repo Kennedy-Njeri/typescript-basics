b23de33c6b8628d729702dfdac0f6e40ba9bad66
Kennedy Njeri <mistakenz123@gmail.com>
2020-11-02 20:07:07 +0300

    instance method modifiers in classes

---
+++ classes.ts
@@ -0,0 +1,36 @@
+// instance method modifiers
+class Vehicle {
+    // method can be called any where any time
+    public driveFast(): void {
+        console.log("drive fast")
+    }
+
+    // can be called by other methods in this class, or by other methods in child class
+    protected driveSlow(): void {
+        console.log("to avoid accidents")
+    }
+}
+
+
+
+// run ts-node classes.ts
+
+
+// inheritance of classes
+class Car extends Vehicle{
+
+    // theis method can only be called by other methods in the class
+    private drive(): void {
+        console.log("fast it is")
+    }
+
+    startDriving(): void{
+        this.drive()
+        this.driveSlow()
+    }
+}
+
+
+const car = new Car()
+
+car.startDriving()

3d880271a7948990bc95ffb59870a9202bfd19a8
Kennedy Njeri <mistakenz123@gmail.com>
2020-11-03 23:00:18 +0300

    use faker in the project

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59938" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63158" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
